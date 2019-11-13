9ce278dc79b0bc47a98dbe6cfc975066511a68b3
Limm-jk <57378834+Limm-jk@users.noreply.github.com>
2019-11-14 02:08:39 +0900

    book tools

--- OOP_termProject/Model.cpp
+++ OOP_termProject/Model.cpp
@@ -22,6 +22,31 @@ public:
 
 
 	//book 수정, 검색, 삭제
+	bool bookInsert(std::string title, std::string author, std::string date, int ISBN) {
+		Book newbook(title, author, date, ISBN);
+		booklist.push_back(newbook);
+	}
+
+	bool bookSearch(std::string title) {
+		bklistItr = booklist.begin();//초기화
+		while (bklistItr != booklist.end()) {
+			if ((*bklistItr).getBookTitle == title) {
+				return true;
+			}
+			++bklistItr;
+		}
+		bklistItr = booklist.begin();//찾는데 실패 시에 초기화 후 리턴
+		return false;
+	}
+
+	bool bookDelete(std::string title) {
+		if (bookSearch(title)) {
+			booklist.erase(bklistItr);
+			return true;
+		}
+		return false;
+	}
+
 	//person 수정, 검색, 삭제
 
 	//파일 업데이트
