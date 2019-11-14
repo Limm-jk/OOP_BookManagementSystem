67ff530bf4d778e54022671806bdd3e8933f7222
cksghks89 <song960705@naver.com>
2019-11-14 19:06:37 +0900

    ㅁㅁㅁ

--- OOP_termProject/View.cpp
+++ OOP_termProject/View.cpp
@@ -189,10 +189,7 @@ public:
 		std::cout << "" << std::endl;
 		std::cout << "" << std::endl;
 		std::cout << "" << std::endl;
-		std::cout << "" << std::endl;+
-			++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
+		std::cout << "" << std::endl;
 
 		std::cout << "2. 도서 삭제를 선택하셨습니다." << std::endl;
 		std::cout << "" << std::endl;
