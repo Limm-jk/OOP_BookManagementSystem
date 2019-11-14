06273021b2ac2e76b70fd4f002da9b446ee6ccea
cksghks89 <song960705@naver.com>
2019-11-14 19:05:17 +0900

    아무거나

--- OOP_termProject/View.cpp
+++ OOP_termProject/View.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
-
+#include<list>
+#include"Object.cpp"
 
 class View {
 
@@ -7,18 +8,18 @@ public:
 	//초기 UI를 보여주는 메소드
 	//각 메소드에서 입력받을 때 입력받는 값에 대한 예외처리 필요
 
-	int initScreen() {
-		int input;
-
+	//로그인 페이지
+	void initScreen(std::string& Id, std::string& passwd) {
+		
 		std::cout << "-------------------------------------------------" << std::endl;
 		std::cout << "" << std::endl;
 		std::cout << "" << std::endl;
 		std::cout << "" << std::endl;
 		std::cout << "" << std::endl;
 		std::cout << "" << std::endl;
-		std::cout << "1. 도서 반납, 대출, 조회" << std::endl;
-		std::cout << "2. 도서 관리" << std::endl;
-		std::cout << "3. 회원 관리" << std::endl;
+		std::cout << "" << std::endl;
+		std::cout << "----------------------로그인---------------------" << std::endl;
+		std::cout << "" << std::endl;
 		std::cout << "" << std::endl;
 		std::cout << "" << std::endl;
 		std::cout << "" << std::endl;
@@ -27,12 +28,14 @@ public:
 		std::cout << "-------------------------------------------------" << std::endl;
 		std::cout << "" << std::endl;
 
-		std::cin >> input;
-
-		return input;
+		std::cout << "ID : ";
+		std::cin >> Id;
+		std::cout << "Passwd : ";
+		std::cin >> passwd;
 	}
 
-	int LoanReturn() {
+	//유저로 로그인
+	int userPage() {
 		int input = 0;
 
 		std::cout << "-------------------------------------------------" << std::endl;
@@ -41,7 +44,7 @@ public:
 		std::cout << "" << std::endl;
 		std::cout << "" << std::endl;
 		std::cout << "" << std::endl;
-		std::cout << "1. 도서 대출" << std::endl;
+		std::cout << "1. 도서 검색 / 대출" << std::endl;
 		std::cout << "2. 도서 반납" << std::endl;
 		std::cout << "" << std::endl;
 		std::cout << "" << std::endl;
@@ -53,19 +56,18 @@ public:
 		std::cout << "" << std::endl;
 
 		std::cin >> input;
-
+		//예외처리 필요
 		return input;
 	}
 
-	void loanBook(std::string& loanId, std::string& bookTitle) {
-
+	void searchBook(std::string& bookTitle) {
 		std::cout << "-------------------------------------------------" << std::endl;
 		std::cout << "" << std::endl;
 		std::cout << "" << std::endl;
 		std::cout << "" << std::endl;
 		std::cout << "" << std::endl;
 		std::cout << "" << std::endl;
-		std::cout << "1. 도서 대출을 선택하셨습니다." << std::endl;
+		std::cout << "1. 도서 검색 / 대출을 선택하셨습니다." << std::endl;
 		std::cout << "" << std::endl;
 		std::cout << "" << std::endl;
 		std::cout << "" << std::endl;
@@ -75,11 +77,34 @@ public:
 		std::cout << "" << std::endl;
 		std::cout << "-------------------------------------------------" << std::endl;
 		std::cout << "" << std::endl;
-		std::cout << "ID를 입력해 주세요 : ";
-		std::cin >> loanId;
-		std::cout << "대출할 책 이름을 입력해주세요 : ";
+		
+		std::cout << "검색할 책을 입력해 주세요 : ";
 		std::cin >> bookTitle;
+	}
 
+	void loanBook(std::list<Book> searchBooklist, int& bookNumber) {
+		std::list<Book>::iterator list = searchBooklist.begin();
+		std::cout << "-------------------------------------------------" << std::endl;
+		std::cout << "고유번호    제목      저자   출판사      대출여부" << std::endl;
+
+		while (list != searchBooklist.end()) {
+			std::cout << (*list).getbookNumber();
+			std::cout << "	";
+			std::cout << (*list).getBookTitle();
+			std::cout << "	";
+			std::cout << (*list).getAuthor();
+			std::cout << "	";
+			std::cout << (*list).getPublisher();
+			std::cout << "	";
+			std::cout << (*list).getLoan() << std::endl;
+			std::cout << "" << std::endl;
+		}
+		std::cout << "" << std::endl;
+		std::cout << "-------------------------------------------------" << std::endl;
+		std::cout << "" << std::endl;
+		
+		std::cout << "대출할 책의 고유번호를 입력해 주세요 : ";
+		std::cin >> bookNumber;
 	}
 
 	void returnBook(std::string& returnId, std::string& returnTitle) {
@@ -100,8 +125,6 @@ public:
 		std::cout << "-------------------------------------------------" << std::endl;
 		std::cout << "" << std::endl;
 
-		std::cout << "ID를 입력해 주세요 : ";
-		std::cin >> returnId;
 		std::cout << "반납할 책 이름을 입력해주세요 : ";
 		std::cin >> returnTitle;
 	}
