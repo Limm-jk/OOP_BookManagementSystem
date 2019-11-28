66e25d0c02f0edadf819bd396ccbbb2555e9a304
Han-Sungyu <mayguy92@gmail.com>
2019-11-28 17:52:45 +0900

    1

--- OOP_termProject/Model.cpp
+++ OOP_termProject/Model.cpp
@@ -102,10 +102,10 @@ class Model {
 			if ((*pslistItr).getId().find(id) != std::string::npos) {	//find(검색어) 함수는 찾는 문자열이 없을 경우 npos라는 상수를 반환함.
 																				//이 경우 찾았기 때문에 npos가 아닌것.
 				person aperson((*pslistItr).getName(), (*pslistItr).getId(), (*pslistItr).getPasswd());//가리키는 객체를 복사
-				aperson.setLoanNumber((*pslistItr).getLoanNumber);
-				aperson.setLoanBook1((*pslistItr).getLoanBook1);
-				aperson.setLoanBook2((*pslistItr).getLoanBook2);
-				aperson.setLoanBook3((*pslistItr).getLoanBook3);
+				aperson.setLoanNumber((*pslistItr).getLoanNumber());
+				aperson.setLoanBook1((*pslistItr).getLoanBook1());
+				aperson.setLoanBook2((*pslistItr).getLoanBook2());
+				aperson.setLoanBook3((*pslistItr).getLoanBook3());
 
 				alist.push_back(aperson);										//찾았으니 복사된 객체를 리스트 뒤에 푸시.
 			}
